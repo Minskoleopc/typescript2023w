b76132c8bcbe2563aa00a289b1673ef2a6ba57f3
unknown <Minskoleopc>
2023-11-06 06:33:22 -0800

    added

---
+++ script.js
@@ -0,0 +1,88 @@
+
+// let a = 10 
+// let b = true
+// let c = "chinmay"
+// let d = ["chinmay","sarika","ram"]
+// let e = {
+//     firstName:"chinmay",
+//     lastName:"deshpande"
+// }
+
+// let mapA  = new Map()
+// mapA.set('name',"chinmay")
+// let setA = new Set()
+// setA.add(23)
+// console.log(setA)
+
+// let a2;
+// console.log(a2)
+
+// let h1 = document.querySelector('h2')
+// console.log(h1)
+
+// class Person {
+//     constructor(fn,ln){
+//         this.firstName = fn 
+//         this.lastName = ln
+//     }
+// }
+// let amol = new Person("chinmay","deshpande")
+// console.log(amol.firstName)
+// console.log(amol.lastName)
+
+
+// program 2
+
+//let a = true
+console.log(typeof(a))
+
+
+// type 
+//int a  = 10 ;
+
+
+// compile time error 
+
+// function add(){
+
+//}
+
+// run time error 
+let names = ["chinmay","shirish","ram"]
+console.log(names[4])
+console.log(10/0)
+
+// logical error
+function addition(a,b){
+    if( typeof a == 'number' && typeof b == 'number'){
+        console.log(a+b)
+    }
+    else {
+        console.log('incorrect input')
+    }
+}
+addition('2','3')
+addition(2,3)
+
+
+// function addition(a:number,b:number){
+//     console.log(a+b)
+// }
+
+addition(2,3)
+// typescript ------> js -------> browser
+// monday     wed   friday   sat  sun  - 7:30 pm  typescript
+// monday     wed   friday   sat  sun  - 8:00 pm  javascript
+// monday     wed   friday   sat  sun  - 8:30 pm  python
+// monday     wed   friday   sat  sun  - 9:00 pm  java
+
+// tuesday     thursday   sat  sun  -- cypress/playwright/selenium
+// tuesday     thursday   sat  sun  -- cypress/playwright/selenium
+// tuesday     thursday   sat  sun  -- cypress/playwright/selenium
+// tuesday     thursday   sat  sun  -- cypress/playwright/selenium
+
+
+
+
+
+
