bcce72af398a4360dc8ce5f955e2d2c5cee87693
unknown <Minskoleopc>
2023-11-08 07:05:29 -0800

    added

---
+++ build/script2.js
@@ -0,0 +1,58 @@
+"use strict";
+// program 1
+// admin , Admin, ADMIN
+// enum roles  {
+//     ADMIN, 
+//     CUSTOMER,
+//     SUPPORT,
+//     MANAGER,
+//     EMPLOYEE
+// }
+// if(roles.ADMIN){
+//     console.log("admin")
+// }
+// else if(roles.CUSTOMER){
+//     console.log("customer")
+// }
+// else if(roles.SUPPORT){
+//     console.log("support")
+// }
+// else if(roles.MANAGER){
+//     console.log("manager")  
+// }
+// if(roles.EMPLOYEE){
+//     console.log("emplyee")
+// }
+var Direction;
+(function (Direction) {
+    Direction[Direction["EAST"] = 5] = "EAST";
+    Direction[Direction["SOUTH"] = 6] = "SOUTH";
+    Direction[Direction["WEST"] = 7] = "WEST";
+    Direction[Direction["NORTH"] = 8] = "NORTH";
+})(Direction || (Direction = {}));
+if (Direction.NORTH) {
+    console.log("Delhi");
+    console.log(Direction.NORTH);
+}
+if (Direction.EAST) {
+    console.log("Bengal");
+}
+if (Direction.WEST) {
+    console.log("Mumbai");
+}
+if (Direction.SOUTH) {
+    console.log("Indira point");
+}
+var statusCode;
+(function (statusCode) {
+    statusCode[statusCode["NotFound"] = 404] = "NotFound";
+    statusCode[statusCode["Success"] = 200] = "Success";
+    statusCode[statusCode["Created"] = 201] = "Created";
+    statusCode[statusCode["TimeOut"] = 503] = "TimeOut";
+})(statusCode || (statusCode = {}));
+console.log(statusCode.NotFound);
+let amol = {
+    firstName: "chinmay",
+    lastName: "deshpande",
+    rollNo: 23
+};
